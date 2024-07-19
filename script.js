19cf7683cb2906dff093b1f576d9cab8dae1034a
Stephanie <tiffanieho@outlook.com>
2024-07-19 10:26:10 +0100

    add DOM element

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     let attempts = 0;
+
+
+    const message = document.getElementById('message');
+    const guessInput = document.getElementById('guessInput');
+    const guessButton = document.getElementById('guessButton');
+    const attemptsDisplay = document.getElementById('attempts');
+
 });
